9f4f3dc5d4e7a8a026db65fe662edbe97689e9c4
Mehmood Bhutta <mehmood.78@hotmail.com>
2023-07-13 19:25:08 +0500

    new features

--- polls/urls.py
+++ polls/urls.py
@@ -13,4 +13,11 @@ urlpatterns = [
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    # ex: /polls/choice/
+    path("<int:question_id>/newchoice/", views.newchoice, name="newchoice"),
+    # ex polls/reset
+    path("<int:question_id>/reset/", views.reset, name="reset"),
+    #ex polls/addquestions
+    path("addques/", views.addques, name="addques"),
+    path("newques/", views.newques, name="newques"),
 ]
